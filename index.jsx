ef98e27e210fb7f059b161ee8403b5ec6408724b
ruhaans05 <143101275+ruhaans05@users.noreply.github.com>
2025-07-09 19:45:04 -0400

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Upload from './upload';
+import './style2.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Upload />
+  </React.StrictMode>
+);
